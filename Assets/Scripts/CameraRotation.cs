0e1ba7b23c1e2a03cdc2da6586a4b4955d7e67b7
Adrian <adrianporterocastillo@gmail.com>
2023-02-04 17:01:22 +0100

    Trying to make the camera move

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
