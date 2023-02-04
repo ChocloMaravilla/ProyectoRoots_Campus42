128010ed8957cf02b0485f48ed926f0e0aaa0924
Just A Green Toast <42865757+PabloFon03@users.noreply.github.com>
2023-02-04 17:30:55 +0100

    estan pasando cosas avisen

--- Assets/BoardData.cs
+++ Assets/BoardData.cs
@@ -1,20 +1,31 @@
+using System.IO;
 using UnityEngine;
 
 public class BoardData
 {
 	public Vector2Int size;
 	public int[] rawTiles;
-	public BoardData(Vector2Int _size, int[][] _rawTiles)
+	public int[] rawValueTiles;
+	public BoardData() { }
+	public BoardData(Vector2Int _size, int[][] _rawTiles, int[][] _rawValueTiles)
 	{
 		size = _size;
 		rawTiles = new int[size.x * size.y];
+		rawValueTiles = new int[size.x * size.y];
 		for (int y = 0; y < size.y; y++)
 		{
 			for (int x = 0; x < size.x; x++)
 			{
 				rawTiles[y * size.x + x] = _rawTiles[y][x];
+				rawValueTiles[y * size.x + x] = _rawValueTiles[y][x];
 			}
 		}
+	}
+	public void LoadFromFile(string _jsonPath)
+	{
+		string jsonData = "";
+		using (StreamReader sr = new StreamReader(_jsonPath)) { jsonData = sr.ReadToEnd(); }
+		JsonUtility.FromJsonOverwrite(jsonData, this);
 	}
 	public int[][] GetRawTileMatrix()
 	{
@@ -25,6 +36,19 @@ public class BoardData
 			for (int x = 0; x < size.x; x++)
 			{
 				returnMatrix[y][x] = rawTiles[y * size.x + x];
+			}
+		}
+		return returnMatrix;
+	}
+	public int[][] GetRawTileValueMatrix()
+	{
+		int[][] returnMatrix = new int[size.y][];
+		for (int y = 0; y < size.y; y++)
+		{
+			returnMatrix[y] = new int[size.x];
+			for (int x = 0; x < size.x; x++)
+			{
+				returnMatrix[y][x] = rawValueTiles[y * size.x + x];
 			}
 		}
 		return returnMatrix;
--- Assets/EditorGridManagerScript.cs
+++ Assets/EditorGridManagerScript.cs
@@ -7,12 +7,17 @@ using TMPro;
 public class EditorGridManagerScript : MonoBehaviour
 {
 	public Sprite[] tileSprites;
+	public Sprite[] tileValueSprites;
 	Vector2Int gridSize = new Vector2Int(20, 20);
 	int[][] rawGrid;
+	int[][] rawValueGrid;
 	public GameObject gridTile;
 	Transform cursor;
 	int brushTile;
 	const int maxBrush = 4;
+	bool onValueGrid;
+	int brushValueTile;
+	const int maxValueBrush = 7;
 	string[] tileNames = new string[]
 	{
 		"Eraser >:)",
@@ -23,6 +28,8 @@ public class EditorGridManagerScript : MonoBehaviour
 	};
 	Transform grid;
 	Transform cursorSelection;
+	Transform valueGrid;
+	Transform valueCursorSelection;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,19 +37,30 @@ public class EditorGridManagerScript : MonoBehaviour
     	Camera.main.orthographicSize = Mathf.Max(gridSize.x, gridSize.y) / 2f;
         // Generate Grid
         rawGrid = new int[gridSize.y][];
+        rawValueGrid = new int[gridSize.y][];
         grid = transform.GetChild(1);
+        valueGrid = transform.GetChild(3);
         for (int y = 0; y < gridSize.y; y++)
         {
         	rawGrid[y] = new int[gridSize.x];
+        	rawValueGrid[y] = new int[gridSize.x];
         	for (int x = 0; x < gridSize.x; x++)
 	        {
+	        	rawValueGrid[y][x] = 1;
 	        	Instantiate(gridTile, new Vector2(x, gridSize.y - y - 1), Quaternion.identity, grid);
+	        	SpriteRenderer r = Instantiate(gridTile, new Vector2(x, gridSize.y - y - 1), Quaternion.identity, valueGrid).GetComponent<SpriteRenderer>();
+	        	r.sprite = tileValueSprites[1];
+	        	r.enabled = false;
 	        }
         }
         cursor = transform.GetChild(0);
         cursorSelection = transform.GetChild(2);
         cursorSelection.position = new Vector2((gridSize.x / 2f - 0.5f) * 3f, (gridSize.y / 2f - 0.5f) * 1.9f);
+        valueCursorSelection = transform.GetChild(4);
+        valueCursorSelection.position = new Vector2((gridSize.x / 2f - 0.5f) * 3f, (gridSize.y / 2f - 0.5f) * 1.9f - 1.5f);
         UpdateCursorSelection();
+        valueGrid.gameObject.SetActive(false);
+        valueCursorSelection.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -54,24 +72,59 @@ public class EditorGridManagerScript : MonoBehaviour
         cursorGridPos.x = Mathf.Clamp(Mathf.RoundToInt(cursorPos.x), 0, gridSize.x - 1);
         cursorGridPos.y = Mathf.Clamp(Mathf.RoundToInt(cursorPos.y), 0, gridSize.y - 1);
     	cursor.position = (Vector2)cursorGridPos;
-    	// Paint Tile
-    	if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
+    	// Toggle Grids
+    	if (Input.GetKeyDown(KeyCode.V))
     	{
-    		rawGrid[cursorGridPos.y][cursorGridPos.x] = brushTile;
-    		grid.GetChild((gridSize.y - 1 - cursorGridPos.y) * gridSize.x + cursorGridPos.x).GetComponent<SpriteRenderer>().sprite = GetTileSprite(brushTile);
+    		onValueGrid = !onValueGrid;
+    		grid.gameObject.SetActive(!onValueGrid);
+        	cursorSelection.gameObject.SetActive(!onValueGrid);
+    		valueGrid.gameObject.SetActive(onValueGrid);
+        	valueCursorSelection.gameObject.SetActive(onValueGrid);
+        	UpdateCursorSelection();
+    	}
+    	// Paint Tile
+		if ((Input.GetMouseButton(0) || Input.GetMouseButtonDown(1)) && (rawGrid[cursorGridPos.y][cursorGridPos.x] != 0 || !onValueGrid))
+    	{
+    		if (onValueGrid)
+    		{
+    			rawValueGrid[cursorGridPos.y][cursorGridPos.x] = brushValueTile;
+    			valueGrid.GetChild((gridSize.y - 1 - cursorGridPos.y) * gridSize.x + cursorGridPos.x).GetComponent<SpriteRenderer>().sprite = GetTileValueSprite(brushValueTile);
+    		}
+    		else
+    		{
+    			rawGrid[cursorGridPos.y][cursorGridPos.x] = brushTile;
+    			grid.GetChild((gridSize.y - 1 - cursorGridPos.y) * gridSize.x + cursorGridPos.x).GetComponent<SpriteRenderer>().sprite = GetTileSprite(brushTile);
+    			valueGrid.GetChild((gridSize.y - 1 - cursorGridPos.y) * gridSize.x + cursorGridPos.x).GetComponent<SpriteRenderer>().enabled = brushTile != 0;
+    		}
     	}
     	// Change Brush Tile
     	if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X))
     	{
-    		if (Input.GetKeyDown(KeyCode.Z))
+    		if (onValueGrid)
     		{
-    			brushTile--;
-    			if (brushTile < 0) { brushTile = maxBrush; }
+    			if (Input.GetKeyDown(KeyCode.Z))
+	    		{
+	    			brushValueTile--;
+	    			if (brushValueTile < 0) { brushValueTile = maxBrush; }
+	    		}
+	    		else
+	    		{
+	    			brushValueTile++;
+	    			if (brushValueTile > maxValueBrush) { brushValueTile = 0; }
+	    		}
     		}
     		else
     		{
-    			brushTile++;
-    			if (brushTile > maxBrush) { brushTile = 0; }
+    			if (Input.GetKeyDown(KeyCode.Z))
+	    		{
+	    			brushTile--;
+	    			if (brushTile < 0) { brushTile = maxBrush; }
+	    		}
+	    		else
+	    		{
+	    			brushTile++;
+	    			if (brushTile > maxBrush) { brushTile = 0; }
+	    		}
     		}
     		UpdateCursorSelection();
     	}
@@ -84,15 +137,23 @@ public class EditorGridManagerScript : MonoBehaviour
     		if (!Directory.Exists(customBoardsPath)) { Directory.CreateDirectory(customBoardsPath); }
     		int fileName = 0;
     		while (File.Exists(customBoardsPath + $"{fileName}.json")) { fileName++; }
-    		using (StreamWriter sw = new StreamWriter(customBoardsPath + $"{fileName}.json")) { sw.Write(JsonUtility.ToJson(new BoardData(gridSize, rawGrid))); }
-    		// print(JsonUtility.ToJson(new BoardData(gridSize, rawGrid)));
+    		using (StreamWriter sw = new StreamWriter(customBoardsPath + $"{fileName}.json")) { sw.Write(JsonUtility.ToJson(new BoardData(gridSize, rawGrid, rawValueGrid))); }
     	}
     }
     Sprite GetTileSprite(int _tileIndex) { return tileSprites[_tileIndex + 1]; }
+    Sprite GetTileValueSprite(int _valueIndex) { return tileValueSprites[_valueIndex]; }
 	void UpdateCursorSelection()
 	{
-		cursor.GetComponent<SpriteRenderer>().sprite = GetTileSprite(brushTile == 0 ? -1 : brushTile);
-		cursorSelection.GetChild(0).GetComponent<TextMeshPro>().text = tileNames[brushTile];
-		for (int i = 0; i <= maxBrush; i++) { cursorSelection.GetChild(i + 1).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i == brushTile ? 1 : 0.25f); }
+		if (onValueGrid)
+		{
+			cursor.GetComponent<SpriteRenderer>().sprite = GetTileValueSprite(brushValueTile);
+			for (int i = 0; i <= maxValueBrush; i++) { valueCursorSelection.GetChild(i).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i == brushValueTile ? 1 : 0.25f); }
+		}
+		else
+		{
+			cursor.GetComponent<SpriteRenderer>().sprite = GetTileSprite(brushTile == 0 ? -1 : brushTile);
+			cursorSelection.GetChild(0).GetComponent<TextMeshPro>().text = tileNames[brushTile];
+			for (int i = 0; i <= maxBrush; i++) { cursorSelection.GetChild(i + 1).GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i == brushTile ? 1 : 0.25f); }
+		}
 	}
 }
