2cf9073b099544d9edbb3447e8954c20d557a9d4
Adrian <adrianporterocastillo@gmail.com>
2023-02-05 04:22:44 +0100

    Added Sound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private AudioClip hoverClip, clickClip, arrastrarClip;
+    [SerializeField] private AudioSource audioSource;
+
+    public void FirstSound()
     {
-        
+        audioSource.PlayOneShot(hoverClip);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClickSound()
     {
-        
+        audioSource.PlayOneShot(clickClip);
+    }
+
+    public void DragSound()
+    {
+        audioSource.PlayOneShot(arrastrarClip);
     }
 }
